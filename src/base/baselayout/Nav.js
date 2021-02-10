b2c359f44cca68c433b79f8d6cde6cac062cbc94
Favour Vivian Woka <vivianemma97@gmai.com>
2021-02-10 03:12:34 +0100

    change css library to tailwind css

--- src/base/baselayout/Nav.js
+++ src/base/baselayout/Nav.js
@@ -5,36 +5,119 @@ import  "../../style/Nav.module.css";
 function Nav() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top nav">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">FayVik</Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <nav class="">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div class="relative flex items-center justify-between h-16">
+            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-expanded="false"
+              >
+                <span class="sr-only">Open main menu</span>
+                <svg
+                  class="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                <svg
+                  class="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div class="flex-shrink-0 flex items-center">
+                <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                  <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+              </div>
+              
+              <div class="hidden sm:block sm:ml-6">
+                <div class="flex">
+                  <li className=" mr-5">
+                    <Link
+                      className="text-white px-3 py-2 text-md font-bold"
+                      to="/"
+                    >
+                      About
+                    </Link>
+                  </li>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav m-auto">
-              <li className="nav-item mr-5">
-                <Link className="nav-link" to="/">About</Link>
-              </li>
-              <li className="nav-item mr-5">
-                <Link className="nav-link" to="/">Services</Link>
-              </li>
-              <li className="nav-item mr-5">
-                <Link className="nav-link" to="/">Works</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Blog</Link>
-              </li>
-            </ul>
+                  <li className=" mr-5">
+                    <Link
+                      className="text-white px-3 py-2 text-md font-bold"
+                      to="/"
+                    >
+                      Services
+                    </Link>
+                  </li>
+                  <li className=" mr-5">
+                    <Link
+                      className="text-white px-3 py-2 text-md font-bold"
+                      to="/"
+                    >
+                      Works
+                    </Link>
+                  </li>
+                  <li className="">
+                    <Link
+                      className="text-white px-3 py-2 text-md font-bold"
+                      to="/"
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="hidden sm:hidden">
+          <div class="px-2 pt-2 pb-3 space-y-1">
+            <a
+              href="#"
+              class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Dashboard
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Team
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Projects
+            </a>
+            <a
+              href="#"
+              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Calendar
+            </a>
           </div>
         </div>
       </nav>
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Button from "../base/BaseButton"
-import style from "../style/Home.module.css"
+import React from "react";
+import Button from "../base/BaseButton";
+import style from "../style/Home.module.css";
+import image from "../image/vick.png";
 
-function Home({pageProps}) {
+function Home({ pageProps }) {
   return (
-    <div className={style.container}> 
-      <div className="row m-0 p-0 justify-content-center align-items-center">
+    <div className={style.container}>
+      <div className="flex flex-wrap">
         <div className="col-lg-6 col-md-6 col-sm-12">
-          {/* <Button {...pageProps}>Meat</Button> */}
+          <Button {...pageProps}>Meat</Button>
         </div>
-        <div className="col-lg-6 col-md-6 col-sm-12">
-          <img src="../../image/vick.JPG" width="400"/>
+        <div className="bg-teal-500 md:bg-red-500 lg:bg-teal-500">
+          <img src={image} style={{ width: "100%" }} />
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
